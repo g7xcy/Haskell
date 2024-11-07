45ebce47a1d274d8d69747d5964de98040520726
Ding, Alex <alex.ding@sap.com>
2024-11-07 22:52:49 +0800

    QSort

--- my_qsort.hs
+++ my_qsort.hs
@@ -1,7 +1,14 @@
+addToSmallOrBig :: Ord a => a -> (([a], [a]) -> a -> ([a], [a]))
+addToSmallOrBig pivot (small, big) x
+  | x < pivot = (x : small, big)
+  | otherwise = (small, x : big)
+
 my_qsort :: Ord a => [a] -> [a]
 my_qsort [] = []
 my_qsort [x] = [x]
-my_qsort (x:xs) = my_qsort [y | y <- xs, y < x] ++ [x] ++ my_qsort [y | y <- xs, y >= x]
+my_qsort (x:xs) = my_qsort small ++ [x] ++ my_qsort big
+    where (small, big) = foldl (addToSmallOrBig x) ([], []) xs
+
 
 main = do
-    putStrLn( show( my_qsort [10000000, 10000000-1..1]))
+    putStrLn( show( my_qsort [100, 99..1]))
