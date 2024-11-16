2a921665b2e6cfe3a19dc314188646531e2d221b
Ding, Alex <alex.ding@sap.com>
2024-11-17 02:40:23 +0800

    Lambda!

--- myFlip.hs
+++ myFlip.hs
@@ -1,7 +1,8 @@
 myFlip :: (a -> b -> c) -> (b -> a -> c)
 -- myFlip f = g
 --     where g x y = f y x
-myFlip f y x = f x y
+-- myFlip f y x = f x y
+myFlip f = \x y -> f y x
 
 f :: a -> a -> a
 f x y = x
