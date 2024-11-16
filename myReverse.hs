f35080bb101363ab055b877d28a9661630663c99
Ding, Alex <alex.ding@sap.com>
2024-11-17 03:54:03 +0800

    I have a very very good implementation of reverse :)

--- myReverse.hs
+++ myReverse.hs
@@ -1,6 +1,10 @@
 myReverse :: [a] -> [a]
-myReverse []     = []
-myReverse (x:xs) = myReverse xs ++ [x]
+-- myReverse []     = []
+-- myReverse (x:xs) = myReverse xs ++ [x]
+
+-- myReverse = foldl (\acc x -> x : acc) []
+
+myReverse = foldl (flip (:)) []
 
 main = do
     print(myReverse [1..10])
