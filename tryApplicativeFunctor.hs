c0a06d3e7d65801d91587a275161e7c51d05f652
Ding, Alex <alex.ding@sap.com>
2024-12-12 01:50:00 +0800

    Add examples for understanding Functor and Applicative Functor in Haskell

--- tryApplicativeFunctor.hs
+++ tryApplicativeFunctor.hs
@@ -1,4 +1,25 @@
+-- How to understand ((->) r) as Applicative Functor?
+-- First, understand that r is type, not value.
+-- (f <$> g <*> h) creates a calculation tree, where
+-- (f <$> g) means f is the root node of the sub tree
+-- (g <*> h) means g and h are of the same level under f, that
+--    f
+--   / \
+-- g     h
+
+-- Or from the defination:
+-- pure x = \_ -> x = const x
+-- f <*> g = \x -> (f x) (g x), where
+-- f :: (r -> (a -> b))
+-- g :: (r -> a), thus
+-- (\x -> g x) :: a, where x is the value of type r, and
+-- (\x -> f x) :: a -> b, where x is the value of type r, thus
+-- (\x -> (f x) (g x)) :: b, means that "result of applying (a -> b) to a is b"
 main = do
+    print $ (+) <$> (*97) <*> (+101) $ 5
+    print $ (+) ((*) 97 5) ((+) 101 5)
+    print $ (97 * 5) + (101 + 5)
+
     print $ pure (*3) <*> Just 5
     print $ pure(replicate 10) <*> "HelloWorld"
     print $ pure(replicate 10) <*> ["HelloWorld"]
---
+++ tryFunctor.hs
@@ -0,0 +1,16 @@
+-- How to understand function ((->) r) as Functor?
+-- First, functor must implement fmap function, where fmap :: (Functor f) -> (a -> b) -> f a -> f b
+-- means that lift a function from (a -> b) to (f a -> f b).
+-- For function ((->) r), the fmap is compose
+-- fmap f g = f . g, because
+-- f :: (a -> b)
+-- g :: (f a) => ((->) r a) => (r -> a)
+-- fmap f g :: (f b) => ((->) r b) => (r -> b)
+-- f . g :: (r -> a) . (a -> b) => (r -> b)
+
+-- Or just understand it by thinking g as "give an input of type r, create an output of type a"
+-- and f as "give an input of type a, create an output of type b".
+-- For fmap, think it as "apply the function f on the value in the functor g, which is the value of type a",
+-- then the result is (r -> "apply (a -> b) on a"), means that "give an input of type r, create an output of type b"
+main = do
+    print $ (+1) <$> (*7) $ 10
