b2e5d1e3a22b38c4586e26410fb30b2e0bc4ce11
Ding, Alex <alex.ding@sap.com>
2024-12-16 14:17:48 +0800

    Add All newtype with Semigroup and Monoid instances in Haskell

--- myMoniodProduct.hs
+++ myMoniodProduct.hs
@@ -6,8 +6,22 @@ instance (Num a) => Semigroup (Product a) where
 instance (Num a) => Monoid (Product a) where
   mempty = Product 1
 
+newtype All = All {getAll :: Bool}
+
+instance Semigroup All where
+  (<>) (All m) (All n) = All (m && n)
+
+instance Monoid All where
+  mempty = All True
+
 main = do
   print $ getProduct . Product <$> [1 .. 10]
   print $ getProduct . mconcat $ Product <$> [1 .. 5]
   print $ getProduct $ Product 10 <> Product 130
   print $ getProduct (mempty :: Product Int)
+
+  print $ getAll (mempty :: All)
+  print $ getAll . All <$> replicate 4 True
+  print $ getAll . All <$> [True, True, True, False]
+  print $ getAll . mconcat $ All <$> replicate 4 True
+  print $ getAll . mconcat $ All <$> [True, True, True, False]
